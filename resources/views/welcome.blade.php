9a5a35cf9392a3b74b1c3eba864872b5adafbd95
Davi Torelli <davitorelli@outlook.com>
2025-02-23 13:45:33 -0300

    tela login

--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <h1 class="text-xl text-red-500">Hello world</h1>
-    <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
-        <flux:radio value="light" icon="sun" />
-        <flux:radio value="dark" icon="moon" />
-    </flux:radio.group>
-</x-layout>
