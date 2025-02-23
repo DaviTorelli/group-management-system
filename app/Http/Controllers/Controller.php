60c18f6dc1d33a954944562b4267d1b4f3d8a975
Davi Torelli <davitorelli@outlook.com>
2025-02-23 13:45:45 -0300

    example

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,9 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+
+
+    // throw ValidationException::withMessages([
+    //     'email' => __('These credentials do not match our records.'),
+    //   ]);
 }
