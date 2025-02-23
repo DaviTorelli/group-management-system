cc184e81d8ac91406c46cfb312b853452c68d97b
Davi Torelli <davitorelli@outlook.com>
2025-02-23 14:37:59 -0300

    clear

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -29,13 +29,3 @@
 
   </form>
 </div>
-
-<!-- <div>
-    <h1 class="text-xl text-red-500">Hello world</h1>
-    <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
-        <flux:radio value="light" icon="sun" />
-        <flux:radio value="dark" icon="moon" />
-    </flux:radio.group>
-
-    <flux:input wire:model="email" label="Email" />
-</div> -->
