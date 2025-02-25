62825c81d899350d99519dc144c99400240b5dc1
Davi Torelli <davitorelli@outlook.com>
2025-02-25 09:22:01 -0300

    lista de colaboradores + delete

--- app/Livewire/Private/Employee/EmployeesList.php
+++ app/Livewire/Private/Employee/EmployeesList.php
@@ -3,15 +3,51 @@
 namespace App\Livewire\Private\Employee;
 
 //* Importações Livewire
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+
+//* Importações de models
+use App\Models\Employee;
 
 class EmployeesList extends Component
 {
   #[Layout("components.layouts.private")]
 
+  public $sortBy = 'name';
+  public $sortDirection = 'desc';
+
+  public function sort($column)
+  {
+    if ($this->sortBy === $column) {
+      $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      $this->sortBy = $column;
+      $this->sortDirection = 'asc';
+    }
+  }
+
+  #[Computed()]
+  public function employees()
+  {
+    return Employee::query()
+      ->tap(fn($query) => $this->sortBy ? $query->orderBy($this->sortBy, $this->sortDirection) : $query)
+      ->paginate(5);
+  }
+
+  public function destroy(Int $id)
+  {
+    try {
+      $employee = Employee::findOrFail($id)->delete();
+      session()->flash("success", "Colaborador excluído com sucesso!");
+    } catch (\Exception $e) {
+      session()->flash("error", "Erro ao excluir colaborador");
+      return;
+    }
+  }
+
   public function render()
   {
-    return view('livewire.private.employee.employees-list');
+    return view('livewire.private.employee.employees-list', ['employees' => $this->employees()]);
   }
 }
--- resources/views/livewire/private/employee/employees-list.blade.php
+++ resources/views/livewire/private/employee/employees-list.blade.php
@@ -1,6 +1,134 @@
 <flux:main container>
-    <flux:heading size="xl" level="1">Colaboradores</flux:heading>
-    <flux:subheading size="lg" class="mb-6">Veja todos os colaboradores cadastrados em seu sistema.</flux:subheading>
+  <flux:heading size="xl" level="1">Colaboradores</flux:heading>
+  <flux:subheading size="lg" class="mb-6">Veja todos os colaboradores cadastrados em seu sistema.</flux:subheading>
 
-    <flux:separator variant="subtle" />
+  <flux:separator variant="subtle" />
+
+  @if (session()->has('error'))
+  <div
+    x-data="{ show: true }"
+    x-show="show"
+    class="flex items-center justify-between p-4 mb-4 border rounded-lg text-rose-700 bg-rose-100 border-rose-400 dark:bg-rose-900 dark:text-rose-300">
+    <span>{{ session('error') }}</span>
+    <flux:button icon="x-mark" variant="ghost" @click="show = false" class="text-rose-900 dark:text-rose-300 hover:text-rose-700" />
+  </div>
+  @endif
+
+  @if (session()->has('success'))
+  <div
+    x-data="{ show: true }"
+    x-show="show"
+    class="flex items-center justify-between p-4 mb-4 text-green-700 bg-green-100 border border-green-400 rounded-lg dark:bg-green-900 dark:text-green-300">
+    <span>{{ session('success') }}</span>
+    <flux:button icon="x-mark" variant="ghost" @click="show = false" class="text-green-900 dark:text-green-300 hover:text-green-700" />
+  </div>
+  @endif
+
+  <div class="container pt-4">
+    <div class="overflow-hidden bg-white rounded-lg shadow-md dark:bg-neutral-900">
+      <div class="flex items-center justify-end p-4">
+        <a class="px-4 py-2 font-bold text-white rounded bg-lime-600 hover:bg-lime-700" href="{{ route('employees.create') }}">
+          + Adicionar Colaborador
+        </a>
+      </div>
+
+      @if ($employees->isEmpty())
+      <div class="p-4 text-center text-gray-600 dark:text-gray-300">
+        Nenhum colaborador encontrado.
+      </div>
+      @else
+      <table class="w-full text-left border-collapse">
+        <thead>
+          <tr class="text-sm leading-normal text-gray-600 bg-gray-100 dark:bg-gray-700 dark:text-gray-300">
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('id')">
+              <span class="flex flex-row items-center gap-1">
+                ID
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('name')">
+              <span class="flex flex-row items-center gap-1">
+                Nome
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('email')">
+              <span class="flex flex-row items-center gap-1">
+                E-mail
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('cpf')">
+              <span class="flex flex-row items-center gap-1">
+                CPF
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('created_at')">
+              <span class="flex flex-row items-center gap-1">
+                Criado em
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 cursor-pointer" wire:click="sort('updated_at')">
+              <span class="flex flex-row items-center gap-1">
+                Última atualização
+                <flux:icon.arrows-up-down variant="micro" />
+              </span>
+            </th>
+            <th class="px-6 py-3 text-end">
+              Ações
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text-sm text-gray-700 divide-y divide-gray-200 dark:text-gray-300 dark:divide-gray-600">
+          @foreach ($employees as $employee)
+          <tr class="hover:bg-gray-50 dark:hover:bg-gray-600" key="{{ $employee->id }}">
+            <td class="px-6 py-3">{{ $employee->id }}</td>
+            <td class="px-6 py-3">{{ $employee->name }}</td>
+            <td class="px-6 py-3">{{ $employee->email }}</td>
+            <td class="px-6 py-3">{{ $employee->cpf }}</td>
+            <td class="px-6 py-3">{{ $employee->created_at->format('d/m/Y - H:i') }}</td>
+            <td class="px-6 py-3">{{ $employee->updated_at->format('d/m/Y - H:i') }}</td>
+            <td class="p-2 text-right">
+              <span class="flex items-center justify-end gap-2">
+                <flux:button
+                  href="/employees/edit/{{ $employee->id }}"
+                  icon="pencil-square"
+                  variant="primary" />
+
+                <flux:modal.trigger :name="'delete-employee-'.$employee->id">
+                  <flux:button icon="trash" variant="danger" />
+                </flux:modal.trigger>
+              </span>
+
+            </td>
+          </tr>
+          <flux:modal :name="'delete-employee-'.$employee->id" class="min-w-[22rem] space-y-6">
+            <div>
+              <flux:heading size="lg">Deletar colaborador?</flux:heading>
+
+              <flux:subheading>
+                <p>Essa é uma ação irreversível.</p>
+              </flux:subheading>
+            </div>
+
+            <div class="flex gap-2">
+              <flux:spacer />
+
+              <flux:modal.close>
+                <flux:button variant="ghost">Cancelar</flux:button>
+              </flux:modal.close>
+
+              <flux:button wire:click="destroy({{ $employee->id }})" variant="danger">Excluir</flux:button>
+            </div>
+          </flux:modal>
+          @endforeach
+        </tbody>
+      </table>
+      <div class="p-4">{{ $employees->links() }}</div>
+      @endif
+    </div>
+  </div>
+
 </flux:main>
