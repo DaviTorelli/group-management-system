666666ae7471554c2ef5802291ec9806606404a3
Davi Torelli <davitorelli@outlook.com>
2025-02-26 18:32:15 -0300

    dark theme

--- resources/views/livewire/private/employee/view-employee.blade.php
+++ resources/views/livewire/private/employee/view-employee.blade.php
@@ -5,40 +5,46 @@
   <flux:separator variant="subtle" />
 
   <div wire:loading wire:target="mount">
-    <span class="text-gray-500">Loading...</span>
+    <span class="text-neutral-500">Loading...</span>
   </div>
 
-  <div class="border-t border-gray-200" wire:loading.remove>
+  <div class="py-2" wire:loading.remove>
     <dl>
-      <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.user class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-neutral-50 dark:bg-neutral-800 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.user class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           Nome
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $this->employee->name }}</dd>
+        <dd class="mt-1 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
+          {{ $this->employee->name }}
+        </dd>
       </div>
-      <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.envelope class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-white dark:bg-neutral-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.envelope class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           E-mail
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $this->employee->email }}</dd>
+        <dd class="mt-1 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
+          {{ $this->employee->email }}
+        </dd>
       </div>
-      <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.identification class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-neutral-50 dark:bg-neutral-800 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.identification class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           CPF
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $this->employee->cpf }}</dd>
+        <dd class="mt-1 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
+          {{ $this->employee->cpf }}
+        </dd>
       </div>
-      <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.building-office class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-white dark:bg-neutral-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.building-office class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           Unidade
         </dt>
-        <dd class="mt-1 space-y-2 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        <dd class="mt-1 space-y-2 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
           <p>
-            <span class="font-semibold">Razão Social:</span> {{ $this->employee->unit->fantasy_name }}
+            <span class="font-semibold">Razão Social:</span> {{ $this->employee->unit->legal_name }}
           </p>
           <p>
             <span class="font-semibold">Nome Fantasia:</span> {{ $this->employee->unit->fantasy_name }}
@@ -48,22 +54,32 @@
           </p>
         </dd>
       </div>
-      <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.flag class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-neutral-50 dark:bg-neutral-800 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.flag class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           Bandeira
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $this->employee->unit->flag->name }}</dd>
+        <dd class="mt-1 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
+          {{ $this->employee->unit->flag->name }}
+        </dd>
       </div>
-      <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="flex items-center text-sm font-medium text-gray-500">
-          <flux:icon.wallet class="mr-2 text-gray-400 size-5" />
+      <div class="px-4 py-5 bg-white dark:bg-neutral-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="flex items-center text-sm font-medium text-neutral-500 dark:text-neutral-300">
+          <flux:icon.wallet class="mr-2 text-neutral-400 dark:text-neutral-500 size-5" />
           Grupo Econômico
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        <dd class="mt-1 text-sm text-neutral-900 dark:text-neutral-100 sm:mt-0 sm:col-span-2">
           {{ $this->employee->unit->flag->economicGroup->name }}
         </dd>
       </div>
     </dl>
   </div>
+
+  <flux:button
+    class="w-24"
+    href="{{ route('employees') }}"
+    icon="arrow-uturn-left">
+    Voltar
+  </flux:button>
+
 </flux:main>
