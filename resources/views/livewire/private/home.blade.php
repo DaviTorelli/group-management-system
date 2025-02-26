7b0d99b269d0e32d82736670fabfe82fe0131b93
Davi Torelli <davitorelli@outlook.com>
2025-02-26 13:47:22 -0300

    home

--- resources/views/livewire/private/home.blade.php
+++ resources/views/livewire/private/home.blade.php
@@ -1,5 +1,9 @@
 <flux:main container>
-    <flux:heading size="xl" level="1">Olá, {{ $name }} </flux:heading>
+    <flux:heading size="xl" level="1">Olá, {{ $name }} 👋</flux:heading>
 
-    <flux:subheading size="lg" class="mb-6">texto introdutório</flux:subheading>
+    <flux:subheading size="lg">
+        Seja bem-vindo ao <strong>Group Management System (GMS)</strong>! Aqui você pode gerenciar <strong>grupos econômicos, bandeiras, unidades e colaboradores</strong> de forma simples e eficiente.
+        Além disso, o sistema permite gerar relatórios detalhados sobre seus colaboradores.
+    </flux:subheading>
+
 </flux:main>
